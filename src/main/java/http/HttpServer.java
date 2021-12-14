edd1a93d9432c3b92eb7af191c0307bd3b9dd79f
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-14 14:02:07 +0800

    write http server

--- src/main/java/http/HttpServer.java
+++ src/main/java/http/HttpServer.java
@@ -3,43 +3,34 @@ package http;
 import java.io.*;
 import java.net.*;
 
-
+// Server should be done. need to do the thread
 public class HttpServer {
 
+    int port;
+    String webroot;
 
-public static void main(String[] args) {
-    int portno = 8080;
-
-    try {
-        ServerSocket serverSocket = new ServerSocket(portno);
-        Socket socket = serverSocket.accept();
-
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
-
-
-        String html = "<html><title>http server</title><body><h1>this page was served by my server<h1></body></html>";
-        final String CRLF = "\r\n";
-        String response = 
-                "HTTP/1.1 200 OK" + CRLF + //http response code
-                "Content-Length: " +  html.getBytes().length + CRLF + //header
-                CRLF+
-                html+
-                CRLF+CRLF;
-
-        outputStream.write(response.getBytes());
-
-
-
-        inputStream.close();
-        outputStream.close();
-        socket.close();
-        serverSocket.close();
-    } 
-    catch (IOException e) {
-        e.printStackTrace();
+    public HttpServer (int port, String webroot){
+        this.port = port;
+        this.webroot = webroot;
     }
-}
-  
+
+    public void startServer(){
+        try {
+            ServerListenerThread serverListenerThread = new ServerListenerThread(this.port, this.webroot);
+            serverListenerThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public static void main(String[] args) {
+        
+    //     try {
+    //         ServerListenerThread serverListenerThread = new ServerListenerThread(8080, "./index");
+    //         serverListenerThread.start();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
 
