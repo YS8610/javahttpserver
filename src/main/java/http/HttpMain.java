2f996cbe78a819cfe9704fa350c78649c70e049a
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-14 14:59:32 +0800

    write command line arguments parser and check if directory exists in main

--- src/main/java/http/HttpMain.java
+++ src/main/java/http/HttpMain.java
@@ -1,10 +1,66 @@
 package http;
 
+import java.io.*;
+
 
 public class HttpMain 
 {
+    // function for task 4
+    static public boolean checkFolder(String pathDirectory){
+        File file = new File(pathDirectory);
+        if (file.isDirectory()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    //command line arguments parser
+    static public String[] parseInput(String[] cmdlineArgs){
+        String port = "8080"; //default port
+        String docRoot = "./static"; //default directory
+        String[] cmdArg = {port,docRoot};
+        
+        if (null==cmdlineArgs || cmdlineArgs.length == 1){
+            return cmdArg;
+        }
+        else if (cmdlineArgs.length ==2|| cmdlineArgs.length ==3 && null!=cmdlineArgs){
+            if (cmdlineArgs[0] == "--port")
+            {
+                cmdArg[0] = cmdlineArgs[1];
+            }
+            else if (cmdlineArgs[0] == "--docRoot"){
+                cmdArg[1] = cmdlineArgs[1];
+            }
+        }
+        else if (cmdlineArgs.length ==4 && null!=cmdlineArgs){
+            if (cmdlineArgs[0] == "--port" && cmdlineArgs[2] == "--docRoot"){
+                cmdArg[0] = cmdlineArgs[1];
+                cmdArg[1] = cmdlineArgs[3];
+            }
+            else if (cmdlineArgs[2] == "--docRoot" && cmdlineArgs[0] == "--port"){
+                cmdArg[0] = cmdlineArgs[3];
+                cmdArg[1] = cmdlineArgs[1];
+            }
+        }
+        System.out.println("Port: "+cmdArg[0] +" will be used at directory: "+ cmdArg[1]);
+        return cmdArg;
+    }
+
+
     public static void main( String[] args ){
-        HttpServer server = new HttpServer(8080,"hello");
+        String[] commandArg = parseInput(args);
+
+        int port = Integer.parseInt(commandArg[0]); //no checking of integer. Pls note
+        String dir = "./static";
+        if (checkFolder(dir)){
+            dir = commandArg[1];
+        }
+        else {
+            System.exit(1);
+        }
+
+        HttpServer server = new HttpServer(port,dir);
         server.startServer();
     }
 
