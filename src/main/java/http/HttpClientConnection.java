bdf23f2698cb8706cd7836d506289fa2fc302b2c
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-14 14:04:49 +0800

    work on httpclientconnection

--- src/main/java/http/HttpClientConnection.java
+++ src/main/java/http/HttpClientConnection.java
@@ -1,5 +1,73 @@
 package http;
 
-public class HttpClientConnection {
+import java.io.*;
+import java.net.*;
+
+//Thread for processing client request
+public class HttpClientConnection extends Thread {
+
+    private Socket socket;
+
+    public HttpClientConnection(Socket socket){
+        this.socket = socket;
+    }
+
+
+    @Override
+    public void run(){
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+
+        try {
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+
+            int _byte;
+
+            // while ( (_byte = bufferedInputStream.read()) >=0 ){
+            //     System.out.println((char)_byte);
+            // }
     
+            String html = "<html><title>http server</title><body><h1>this page was served by my server<h1></body></html>";
+            final String CRLF = "\r\n";
+            String response = 
+                    "HTTP/1.1 200 OK" + CRLF + //http response code
+                    "Content-Length: " +  html.getBytes().length + CRLF + //header
+                    CRLF+
+                    html+
+                    CRLF+CRLF;
+
+            outputStream.write(response.getBytes());
+
+            System.out.println( "Processing of connection done");
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (inputStream!= null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    //TODO: handle exception
+                }
+            }
+            if (outputStream!= null){
+                try {
+                    outputStream.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+            }
+            if (socket!= null) {
+                try {
+                    socket.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+            }
+        }
+    }
 }
