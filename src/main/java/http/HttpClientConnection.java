95d71bc94f732cfbee2e4ef156feb6dca34bbd4c
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-14 16:48:30 +0800

    add function to check for request method

--- src/main/java/http/HttpClientConnection.java
+++ src/main/java/http/HttpClientConnection.java
@@ -2,36 +2,76 @@ package http;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 //Thread for processing client request
 public class HttpClientConnection extends Thread {
 
     private Socket socket;
+    private String webroot;
 
-    public HttpClientConnection(Socket socket){
+    public HttpClientConnection(Socket socket,String webroot){
         this.socket = socket;
+        this.webroot = webroot;
     }
 
+// read index file and return as arraylist of string
+    private List<String> readFileBuffer(String filename){
+        String line;
+        List<String> listofString= new ArrayList<>();
+        try (Reader reader = new FileReader(filename)){
+            BufferedReader br = new BufferedReader(reader);
+            while (null != (line = br.readLine())){
+                listofString.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listofString;
+    }
+
+//concatenate list of string together
+    private String openHtmlFile( List<String> listofString){
+        String html="";
+        for (String s : listofString){
+            html += s.trim();
+        }
+        return html;
+    }
+// check the request method
+    private String getMethod(BufferedReader bufferedReader) throws IOException{
+        String input = bufferedReader.readLine();
+        StringTokenizer parse = new StringTokenizer(input);
+        String method = parse.nextToken().toUpperCase();
+        System.out.println(method);
+        return method;
+    }
 
     @Override
     public void run(){
 
         InputStream inputStream = null;
         OutputStream outputStream = null;
-
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        List<String> htmlList = readFileBuffer(this.webroot+"/index.html");
+        String html1 = openHtmlFile(htmlList);
+        final String CRLF = "\r\n";
 
         try {
             inputStream = socket.getInputStream();
             outputStream = socket.getOutputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream) );
 
-            // int _byte;
-
-            // while ( (_byte = bufferedInputStream.read()) >=0 ){
-            //     System.out.println((char)_byte);
-            // }
-    
-            String html = "<html><title>http server</title><body><h1>this page was served by my server<h1></body></html>";
-            final String CRLF = "\r\n";
+            // check if get method is being used
+            if (getMethod(bufferedReader)!="GET"){
+            //     String resp = "HTTP/1.1 405 Method is Not Allowed" + CRLF + CRLF + getMethod(bufferedReader) +" not supported" +CRLF;
+            //     outputStream.write(resp.getBytes());
+                System.out.println("GET method not used");
+            }
+            
+            String html = "<html><link rel='stylesheet' href='style.css'><title>http server</title><body><h1>Hello World</h1><img src='./rainbow.png' width = 100vw><p><a href='./aboutme.html'>About me</a></p></body></html>";
+            System.out.println(html1);
             String response = 
                     "HTTP/1.1 200 OK" + CRLF + //http response code
                     "Content-Length: " +  html.getBytes().length + CRLF + //header
@@ -42,6 +82,7 @@ public class HttpClientConnection extends Thread {
             outputStream.write(response.getBytes());
 
             System.out.println( "Processing of connection done");
+            
         } 
         catch (IOException e) {
             e.printStackTrace();
