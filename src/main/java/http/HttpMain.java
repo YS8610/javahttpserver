5966da3f1b3ee6d68396ce89a2181c0314d9d27b
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-17 17:06:06 +0800

    Rewrite the commandline parser and change default port to 3000

--- src/main/java/http/HttpMain.java
+++ src/main/java/http/HttpMain.java
@@ -1,6 +1,9 @@
 package http;
 
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class HttpMain 
@@ -16,41 +19,30 @@ public class HttpMain
         }
     }
     //command line arguments parser
-    static public String[] parseInput(String[] cmdlineArgs){
-        String port = "8080"; //default port
-        String docRoot = "./static"; //default directory
-        String[] cmdArg = {port,docRoot};
-        
-        if (null==cmdlineArgs || cmdlineArgs.length == 1){
-            return cmdArg;
+        public static String[] cmdParser(String[] cmdarg){
+            List<String> listofArg = new ArrayList<>();
+            String port = "3000"; //default port
+            String docRoot = "./static"; //default directory
+            String[] portDir = {port,docRoot};
+            if (cmdarg.length>1 && cmdarg!=null){
+                listofArg = Arrays.asList(cmdarg);
+                if (listofArg.size()==2){
+                    if (listofArg.contains("--port")) portDir[0] = listofArg.get(1);
+                    if (listofArg.contains("--docRoot")) portDir[1] = listofArg.get(1);
+                }
+                else if (listofArg.size()==4){
+                    int posofPort = listofArg.indexOf("--port");
+                    int posofWebRoot = listofArg.indexOf("--docRoot");
+                    portDir[0] = listofArg.get(posofPort+1);
+                    portDir[1] = listofArg.get(posofWebRoot+1);
+                }
+            }
+            return portDir;
         }
-        else if (cmdlineArgs.length ==2|| cmdlineArgs.length ==3 && null!=cmdlineArgs){
-            if (cmdlineArgs[0].equals("--port") )
-            {
-                cmdArg[0] = cmdlineArgs[1];
-            }
-            else if (cmdlineArgs[0].equals("--docRoot") ){
-                cmdArg[1] = cmdlineArgs[1];
-            }
-        }
-        else if (cmdlineArgs.length ==4 && null!=cmdlineArgs){
-            if (cmdlineArgs[0].equals("--port")  && cmdlineArgs[2].equals("--docRoot") ){
-                cmdArg[0] = cmdlineArgs[1];
-                cmdArg[1] = cmdlineArgs[3];
-            }
-            else if (cmdlineArgs[2].equals("--docRoot") && cmdlineArgs[0].equals("--port")){
-                cmdArg[0] = cmdlineArgs[3];
-                cmdArg[1] = cmdlineArgs[1];
-            }
-        }
-        System.out.println("Port: "+cmdArg[0] +" will be used at directory: "+ cmdArg[1]);
-        return cmdArg;
-    }
-
 
     public static void main( String[] args ){
-        String[] commandArg = parseInput(args);
-
+        // String[] commandArg = parseInput(args);
+        String[] commandArg = cmdParser(args);
         int port = Integer.parseInt(commandArg[0]); //no checking of integer. Pls note
         String dir = "./static";
         if (checkFolder(dir)){
@@ -62,6 +54,6 @@ public class HttpMain
 
         HttpServer server = new HttpServer(port,dir);
         server.startServer();
+        System.out.println("Server starting at port no: "+port+" at directory: "+ dir);
     }
-
 }
