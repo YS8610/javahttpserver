de7e8e09725ba5a954d4f78c82dfc8c7d7502d81
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-18 17:44:18 +0800

    Add function to parse : delimited list of dir + Another function to check if dir is dir, readable and exist

--- src/main/java/http/HttpMain.java
+++ src/main/java/http/HttpMain.java
@@ -1,6 +1,8 @@
 package http;
 
-import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,54 +10,61 @@ import java.util.Arrays;
 
 public class HttpMain 
 {
-    // function for task 4
+// function for task 4
     static public boolean checkFolder(String pathDirectory){
-        File file = new File(pathDirectory);
-        if (file.isDirectory()){
+        Path filePath = Paths.get(pathDirectory);
+        if (Files.isDirectory(filePath) && Files.isReadable(filePath) && Files.exists(filePath) ){
             return true;
         }
         else{
             return false;
         }
     }
-    //command line arguments parser
-        public static String[] cmdParser(String[] cmdarg){
-            List<String> listofArg = new ArrayList<>();
-            String port = "3000"; //default port
-            String docRoot = "./static"; //default directory
-            String[] portDir = {port,docRoot};
-            if (cmdarg.length>1 && cmdarg!=null){
-                listofArg = Arrays.asList(cmdarg);
-                if (listofArg.size()==2){
-                    if (listofArg.contains("--port")) portDir[0] = listofArg.get(1);
-                    if (listofArg.contains("--docRoot")) portDir[1] = listofArg.get(1);
-                }
-                else if (listofArg.size()==4){
-                    int posofPort = listofArg.indexOf("--port");
-                    int posofWebRoot = listofArg.indexOf("--docRoot");
-                    portDir[0] = listofArg.get(posofPort+1);
-                    portDir[1] = listofArg.get(posofWebRoot+1);
-                }
+//command line arguments parser
+    public static String[] cmdParser(String[] cmdarg){
+        List<String> listofArg = new ArrayList<>();
+        String port = "3000"; //default port
+        String docRoot = "./static"; //default directory
+        String[] portDir = {port,docRoot};
+        if (cmdarg.length>1 && cmdarg!=null){
+            listofArg = Arrays.asList(cmdarg);
+            if (listofArg.size()==2){
+                if (listofArg.contains("--port")) portDir[0] = listofArg.get(1);
+                if (listofArg.contains("--docRoot")) portDir[1] = listofArg.get(1);
             }
-            return portDir;
+            else if (listofArg.size()==4){
+                int posofPort = listofArg.indexOf("--port");
+                int posofWebRoot = listofArg.indexOf("--docRoot");
+                portDir[0] = listofArg.get(posofPort+1);
+                portDir[1] = listofArg.get(posofWebRoot+1);
+            }
         }
-
+        return portDir;
+    }
+//parse list of directory/directories
+    static public List<String> parseDir(String paths){
+        List<String> listofDir = new ArrayList<>();
+        if (!paths.contains(":")){
+            listofDir.add(paths);
+        }
+        else{
+            Arrays.stream(paths.split(":")).forEach( (s) -> {if (!s.isBlank()) listofDir.add(s);} ); 
+        }
+        return listofDir;
+    }
+    
     public static void main( String[] args ){
-        // String[] commandArg = parseInput(args);
         String[] commandArg = cmdParser(args);
         int port = Integer.parseInt(commandArg[0]); //no checking of integer. Pls note
-        // String dir = "./static";
-        String dir = commandArg[1];
-        if (checkFolder(dir)){
-            dir = commandArg[1];
-        }
-        else {
-            System.out.println("Directory does not exit.");
-            System.exit(1);
-        }
-
-        HttpServer server = new HttpServer(port,dir);
+        List<String> listofDir = parseDir(commandArg[1]);
+        listofDir.forEach(s -> {
+            if (!checkFolder(s)){
+                System.out.println(s + " does not exist.");
+                System.exit(1);
+            }
+        });
+        HttpServer server = new HttpServer(port,listofDir.get(0));
         server.startServer();
-        System.out.println("Server starting at port no: "+port+" at directory: "+ dir);
+        System.out.println("Server starting at port no: "+port+" at directory: "+ listofDir.get(0));
     }
 }
